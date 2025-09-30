312b00b96e07d4e31f17a390102a6798714b5fd4
yeryung <68985912+yeryung@users.noreply.github.com>
2025-09-30 13:22:36 +0900

    Add files via upload

---
+++ Catalog/src/main/java/com/example/demo/controller/CatalogController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class CatalogController {
+	@GetMapping("/")
+	public String hello() {
+		return "Hello @-@";
+	}
+	
+	@PostMapping("/")
+	public String World() {
+		return "World";
+	}
+	
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
+	@Value("${custom.name:anonymouse}")
+	private String customName;
+	
+	@GetMapping("/whoami")
+	public String profileTest() {
+		return customName + "@" + applicationName;
+	}
+	
+}
